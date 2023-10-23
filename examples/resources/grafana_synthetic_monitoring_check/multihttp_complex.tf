02baa4f614d53de9103b88ab5c6583bd6b0e7506
Russ <8377044+rdubrock@users.noreply.github.com>
2023-10-23 12:38:41 -0800

    update multihttp complex tests

--- examples/resources/grafana_synthetic_monitoring_check/multihttp_complex.tf
+++ examples/resources/grafana_synthetic_monitoring_check/multihttp_complex.tf
@@ -2,7 +2,7 @@ data "grafana_synthetic_monitoring_probes" "main" {}
 
 resource "grafana_synthetic_monitoring_check" "multihttp" {
   job     = "multihttp complex"
-  target  = "https://www.an_auth_endpoint.com"
+  target  = "https://www.an-auth-endpoint.com"
   enabled = false
   probes = [
     data.grafana_synthetic_monitoring_probes.main.probes.Amsterdam,
@@ -14,48 +14,47 @@ resource "grafana_synthetic_monitoring_check" "multihttp" {
     multihttp {
       entries {
         request {
-          method       = "POST"
-          url          = "https://www.an_auth_endpoint.com"
-          query_fields = [{ "name" : "username", "value" : "steve" }, { "name" : "password", "value" : "top_secret" }]
-          body = {
+          method = "POST"
+          url    = "https://www.an-auth-endpoint.com"
+          query_fields {
+            name  = "username"
+            value = "steve"
+          }
+          query_fields {
+            name  = "password"
+            value = "top_secret"
+          }
+          body {
             content_type = "application/json"
           }
         }
-        checks = [
-          {
-            "type" : 0,
-            "subject" : 2,
-            "condition" : 2,
-            "value" : "200"
-          }
-        ]
-        variables = [
-          {
-            "type" : 0,
-            "name" : "accessToken",
-            "expression" : "accessToken"
-          }
-        ]
+        checks {
+          type      = 0
+          subject   = 2
+          condition = 2
+          value     = "200"
+        }
+        variables {
+          type       = 0
+          name       = "accessToken"
+          expression = "data.accessToken"
+        }
       }
       entries {
-        request = {
+        request {
           method = "GET"
-          url    = "https://an_endpoint_that_requires_auth",
-          headers = [
-            {
-              "name" : "Authorization",
-              "value" : "Bearer $${accessToken}"
-            }
-          ]
-        }
-        checks = [
-          {
-            "type" : 1,
-            "condition" : 6,
-            "expression" : "result",
-            "value" : "expected"
+          url    = "https://www.an-endpoint-that-requires-auth.com"
+          headers {
+            name  = "Authorization"
+            value = "Bearer $${accessToken}"
           }
-        ]
+        }
+        checks {
+          type       = 1
+          condition  = 6
+          expression = "result"
+          value      = "expected"
+        }
       }
     }
   }
